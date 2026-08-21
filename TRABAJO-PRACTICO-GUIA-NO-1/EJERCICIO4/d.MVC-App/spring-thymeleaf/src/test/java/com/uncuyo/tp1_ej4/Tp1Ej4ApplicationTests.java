7c214a386dda854551f23a9b97d27656304bf5c1
Francisco Alba <127322698+franualba@users.noreply.github.com>
2026-08-21 13:35:31 -0300

    TP1-EJ4-d-spring-thymeleaf

---
+++ TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO4/d.MVC-App/spring-thymeleaf/src/test/java/com/uncuyo/tp1_ej4/Tp1Ej4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncuyo.tp1_ej4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1Ej4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
