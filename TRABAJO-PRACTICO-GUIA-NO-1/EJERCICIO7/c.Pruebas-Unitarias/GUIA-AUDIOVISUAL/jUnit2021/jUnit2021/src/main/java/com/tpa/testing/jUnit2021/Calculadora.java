71e35115114c2e68f1d393a919dc67658260ae83
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-07 17:13:21 -0300

    Agrego nuevas pruebas al unit-test.

--- TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/main/java/com/tpa/testing/jUnit2021/Calculadora.java
+++ TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/main/java/com/tpa/testing/jUnit2021/Calculadora.java
@@ -4,7 +4,23 @@ public class Calculadora {
     
     
     
-    public int sumar(int a, int b) {
+    public Integer sumar(Integer a, Integer b) {
+        if (a == null) {
+            a = 0;
+        }
+        
+        if (b == null) {
+            b = 0;
+        }
         return a+b;
+    }
+    
+    public Double dividir(Double numerador, Double denominador) throws Exception {
+        if (denominador == null || denominador == 0) {
+            throw new Exception("denominador inválido");
+        }
+        
+        
+        return numerador/denominador;
     }
 }
--- TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/test/java/com/tpa/testing/jUnit2021/CalculadoraTest.java
+++ TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/test/java/com/tpa/testing/jUnit2021/CalculadoraTest.java
@@ -2,12 +2,17 @@ package com.tpa.testing.jUnit2021;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.OrderAnnotation.class)
 public class CalculadoraTest {
-    
+    Calculadora calc = new Calculadora();
     //Tanto BeforeAll como AfterAll deben ser static
     @BeforeAll
     public static void beforeAll() {
@@ -30,21 +35,40 @@ public class CalculadoraTest {
     }
     
     @Test
+    @Order(1)
     public void testSumar() {
         int a=2, b=3, c=5, resultado=0;
-        
-        Calculadora calc = new Calculadora();
         resultado = calc.sumar(a, b);
         
         System.out.println("resultado " + resultado);
     }
     
     @Test
-    public void testSumar1() {
-        int a=2, b=3, c=5, resultado=0;
+    @Order(2)
+    public void testDividirDenominadorCero() {
+        Double a=2.0,b=3.0,c=5.0, resultado = 0.0 ,esperado = 0.6666666666666666;
+        try {
+            resultado = calc.dividir(a, b);
+        } catch (Exception ex) {
+            System.getLogger(CalculadoraTest.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
         
-        Calculadora calc = new Calculadora();
-        resultado = calc.sumar(a, b);
+        Assertions.assertEquals(esperado, resultado);
+        
+        System.out.println("resultado " + resultado);
+    }
+    
+    @Test
+    @Order(3)
+    public void testDividirAssertTrue() {
+        Double a=2.0,b=3.0,c=5.0, resultado = 0.0 ,esperado = 0.6666666666666666;
+        try {
+            resultado = calc.dividir(a, b);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        Assertions.assertTrue(esperado != resultado);
         
         System.out.println("resultado " + resultado);
     }
