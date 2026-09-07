baf7856f252537d2ecf18d632ef61c92ab579e60
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-07 17:40:46 -0300

    Agrego @RepeatedTest.

--- TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/test/java/com/tpa/testing/jUnit2021/CalculadoraTest.java
+++ TRABAJO-PRACTICO-GUIA-NO-1/EJERCICIO7/c.Pruebas-Unitarias/GUIA-AUDIOVISUAL/jUnit2021/jUnit2021/src/test/java/com/tpa/testing/jUnit2021/CalculadoraTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.test.annotation.Repeat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.OrderAnnotation.class)
 public class CalculadoraTest {
@@ -25,13 +28,13 @@ public class CalculadoraTest {
     }
     
     @BeforeEach
-    public void beforeEach() {
-        System.out.println("beforeEach");
+    public void beforeEach(TestInfo testInfo) {
+        System.out.println("beforeEach " + testInfo.getDisplayName());
     }
     
     @AfterEach
-    public void afterEach() {
-        System.out.println("afterEach");
+    public void afterEach(TestInfo testInfo) {
+        System.out.println("afterEach "  + testInfo.getDisplayName());
     }
     
     @Test
@@ -60,10 +63,30 @@ public class CalculadoraTest {
     
     @Test
     @Order(3)
-    public void testDividirAssertTrue() {
-        Double a=2.0,b=3.0,c=5.0, resultado = 0.0 ,esperado = 0.6666666666666666;
+    @RepeatedTest(5)
+    public void testDividirCheckException() {
+        Double a=2.0,b=0.0,c=5.0, resultado = 0.0 ,esperado = 0.6666666666666666;
         try {
-            resultado = calc.dividir(a, b);
+            Exception ex = Assertions.assertThrows(Exception.class, ()->{
+                calc.dividir(a, b);
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        Assertions.assertTrue(esperado != resultado);
+        
+        System.out.println("resultado " + resultado);
+    }
+    
+    @Test
+    @Order(4)
+    public void testDividirCheckException1() {
+        Double a=2.0,b=0.0,c=5.0, resultado = 0.0 ,esperado = 0.6666666666666666;
+        try {
+            Exception ex = Assertions.assertThrows(Exception.class, ()->{
+                calc.dividir(a, b);
+            });
         } catch (Exception ex) {
             ex.printStackTrace();
         }
