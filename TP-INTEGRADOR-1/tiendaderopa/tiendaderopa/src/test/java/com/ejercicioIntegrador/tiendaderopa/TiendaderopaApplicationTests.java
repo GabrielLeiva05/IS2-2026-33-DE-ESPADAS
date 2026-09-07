c7a5980fecef3d11f586699d494572ed1e278142
Joaquin Villegas <cocoproman11@gmail.com>
2026-09-07 16:18:35 -0300

    Creacion Estructura del Proyecto

---
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/test/java/com/ejercicioIntegrador/tiendaderopa/TiendaderopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicioIntegrador.tiendaderopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaderopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
