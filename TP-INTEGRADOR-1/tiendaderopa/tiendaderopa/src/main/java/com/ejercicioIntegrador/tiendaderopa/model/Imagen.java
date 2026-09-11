3c7dbb56093c950e06cb3269d7af0c56a11c9aa9
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-11 08:26:58 -0300

    Modifico la clase Imagen.java para que id sea String con UUID.

--- TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/Imagen.java
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/Imagen.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 //import org.hibernate.envers.Audited;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -16,17 +17,29 @@ import lombok.Setter;
 public class Imagen {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
+
     @Column(nullable = false, length = 100)
     private String nombre;
+
     @Column()
     private String mime;
-    @Lob @Basic(fetch = FetchType.LAZY)
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
     private byte[] contenido;
+
     private boolean eliminado;
 
     @Enumerated(EnumType.STRING)
     private TipoImagen tipoImagen;
+
+    // Como id es String, generamos un UUID para cada nueva imagen antes de persistirla en la base de datos
+    @PrePersist 
+    public void generarId() {
+        if (id == null) {
+            id = UUID.randomUUID().toString();
+        }
+    }
 
 }
