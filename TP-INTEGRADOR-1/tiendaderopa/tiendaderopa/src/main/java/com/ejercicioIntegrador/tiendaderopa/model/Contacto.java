ef86042055ddfe7514bd18cdcef96f247171595b
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-11 18:50:35 -0300

    Modifico la clase Contacto.java agregando @Inheritance, @Enumerated y @GeneratedValue

--- TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/Contacto.java
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/Contacto.java
@@ -13,19 +13,31 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @Table(name = "contacto")
+// @Inheritance sirve para establecer como se estructuran las
+// tablas de la jerarquía de clases en la base de datos.
+// Con JOINED se crea una tabla para la clase base y una tabla para cada subclase.
+// Solo que las de la subclase comparten la misma PK que la tabla de la clase base,
+// y se relacionan con ella mediante una FK. Pero guardan solo los atributos propios de la subclase.
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Contacto implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false)
     private String id;
 
+    /* @Enumerated(EnumType.STRING) sirve para mapear un tipo de enumerador (enum) 
+    de Java en una columna de una base de datos.
+    indica que el valor del enum se guardará como un String en la base de datos.
+    */
+    @Enumerated(EnumType.STRING)
     @Column
     private TipoContacto tipoContacto;
 
     @Column
     private String observacion;
 
-    @Column
+    @Column(nullable = false)
     private boolean eliminado = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -38,8 +50,9 @@ public class Contacto implements Serializable {
     private Imagen imagen;
     */
 
-    public Contacto(TipoContacto tipoContacto, String observacion) {
+    public Contacto(TipoContacto tipoContacto, String observacion, Persona persona) {
         this.tipoContacto = tipoContacto;
         this.observacion = observacion;
+        this.persona = persona;
     }
 }
