780c8d712e86ba39b0a8b9b3a0b5e71f99914ebc
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-22 17:20:34 -0300

    Agrego enum para orden de compra.

---
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/enumeraciones/EstadoOrdenCompra.java
@@ -0,0 +1,10 @@
+package com.ejercicioIntegrador.tiendaderopa.enumeraciones;
+
+public enum EstadoOrdenCompra {
+    PENDIENTE_COMPLETAR,
+    PENDIENTE_PAGO,
+    PENDIENTE_ENVIO,
+    PENDIENTE_ENTREGA,
+    ENTREGADO,
+    ANULADA
+}
