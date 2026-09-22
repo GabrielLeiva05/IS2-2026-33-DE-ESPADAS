4272d68c7e6dc3d24f0f2a6c815b5500586d532a
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-22 18:11:40 -0300

    Agrego nuevos metodos al repositorio de detalle factura.

--- TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/repository/RepositorioDetalleFactura.java
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/repository/RepositorioDetalleFactura.java
@@ -4,6 +4,14 @@ import com.ejercicioIntegrador.tiendaderopa.model.DetalleFactura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RepositorioDetalleFactura extends JpaRepository<DetalleFactura, String> {
+
+    // Obtener solo los detalles activos
+    List<DetalleFactura> findByEliminadoFalse();
+
+    // Obtener los detalles pertenecientes a una factura específica
+    List<DetalleFactura> findByFacturaIdAndEliminadoFalse(String facturaId);
 }
