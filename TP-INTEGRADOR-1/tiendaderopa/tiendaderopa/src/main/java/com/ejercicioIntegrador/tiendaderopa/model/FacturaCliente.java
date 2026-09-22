ea45e027fb1f73598834f2b62c704860d8522c4a
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-22 16:30:54 -0300

    Agrego anotaciones @Entity y @Table a FacturaCliente.

--- TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/FacturaCliente.java
+++ TP-INTEGRADOR-1/tiendaderopa/tiendaderopa/src/main/java/com/ejercicioIntegrador/tiendaderopa/model/FacturaCliente.java
@@ -1,5 +1,10 @@
 package com.ejercicioIntegrador.tiendaderopa.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table (name = "facturas_clientes")
 public class FacturaCliente extends Factura {
 
     @Override
